405887e16a667e8407bbeca63d86ee6ccb4bdd69
Riley Meyerkorth <rileymeyerkorth@gmail.com>
2024-04-26 18:17:59 -0500

    initial init

---
+++ motion_alarm.ino
@@ -0,0 +1,34 @@
+#include <SoftwareSerial.h>
+#include "TFMini.h"
+
+// Initialize pin names
+#define PIN_TFMINI_RX 11
+#define PIN_TFMINI_TX 10
+#define PIN_BUZZER 13
+
+// Initialize global vars
+SoftwareSerial mySerial(PIN_TFMINI_TX, PIN_TFMINI_RX);      // Uno RX (TFMINI TX), Uno TX (TFMINI RX)
+TFMini tfmini;
+
+void setup() {
+  Serial.begin(9600);
+  while (!Serial); // wait for serial port to connect. Needed for native USB port only
+
+  // Send message to serial
+  Serial.println ("Initializing...");
+
+  // Initialize board pin modes
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(PIN_BUZZER, OUTPUT); // Set buzzer - pin 9 as an output
+
+  //Initialize the data rate for the SoftwareSerial port
+  mySerial.begin(TFMINI_BAUDRATE);
+
+  //Initialize the TF Mini sensor
+  tfmini.begin(&mySerial);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
