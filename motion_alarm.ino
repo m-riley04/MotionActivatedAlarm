9e55bff961218149d573685d0b776a2bf9a8b29c
Riley Meyerkorth <rileymeyerkorth@gmail.com>
2024-04-28 16:22:23 -0500

    update comments

--- motion_alarm.ino
+++ motion_alarm.ino
@@ -6,7 +6,7 @@
 #define PIN_TFMINI_TX 10
 #define PIN_BUZZER 13
 
-// Initialize global vars
+// Initialize global objects
 SoftwareSerial mySerial(PIN_TFMINI_TX, PIN_TFMINI_RX);      // Uno RX (TFMINI TX), Uno TX (TFMINI RX)
 TFMini tfmini;
 
@@ -21,7 +21,6 @@ void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
   pinMode(PIN_BUZZER, OUTPUT); // Set buzzer - pin 9 as an output
 
-  //Initialize the data rate for the SoftwareSerial port
   mySerial.begin(TFMINI_BAUDRATE);
 
   //Initialize the TF Mini sensor
