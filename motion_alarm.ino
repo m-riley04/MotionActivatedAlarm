fed5980af9a18089dfe89f5cda6c55f26ec53c21
Riley Meyerkorth <rileymeyerkorth@gmail.com>
2024-04-28 16:32:15 -0500

    move distance check logic into function

--- motion_alarm.ino
+++ motion_alarm.ino
@@ -16,6 +16,7 @@ uint16_t distanceBuffer = 10;
 
 void countdown_beeps(uint8_t buzzer_pin, int beeps, unsigned int frequency = 1000, unsigned long beepDuration = 100, unsigned long beepGapDuration = 1000);
 uint16_t read_lidar(TFMini &tfmini, unsigned long readDelay = 50);
+void check_distance(uint16_t distance, uint16_t distanceThreshold, uint16_t buffer = 5, int buzzerPin = 9, int ledPin = LED_BUILTIN);
 
 void setup() {
   // Initialize board pin modes
@@ -46,19 +47,7 @@ void loop() {
   uint16_t distance = read_lidar(tfmini);
 
   // Check if something crosses the beam
-  if ((distance+distanceBuffer) < minDistance) {
-    // Turn on the on-board LED
-    digitalWrite(LED_BUILTIN, HIGH);
-
-    // Set off the alarm
-    tone(PIN_BUZZER, 1000);
-  } else {
-    // Turn off the LED
-    digitalWrite(LED_BUILTIN, LOW);
-
-    // Turn off the tone
-    noTone(PIN_BUZZER);
-  }
+  check_distance(distance, minDistance, distanceBuffer, PIN_BUZZER, LED_BUILTIN);
 
   // Print data to serial
   Serial.print("Distance: ");
@@ -69,6 +58,16 @@ void loop() {
 
   // Delay to wait for catchups
   delay(25);
+}
+
+void check_distance(uint16_t distance, uint16_t distanceThreshold, uint16_t buffer = 5, int buzzerPin = 9, int ledPin = LED_BUILTIN) {
+  if ((distance + buffer) < distanceThreshold) {
+    digitalWrite(ledPin, HIGH); // Turn on the on-board LED
+    tone(buzzerPin, 1000); // Turn on buzzer
+  } else {
+    digitalWrite(ledPin, LOW); // Turn off the LED
+    noTone(buzzerPin); // Turn off the tone
+  }
 }
 
 void countdown_beeps(uint8_t buzzerPin, int beeps, unsigned int frequency = 1000, unsigned long beepDuration = 100, unsigned long beepGapDuration = 1000) {
