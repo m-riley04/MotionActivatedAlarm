0cf493c80e0839574446d97d0bfd10150224ad39
Riley Meyerkorth <rileymeyerkorth@gmail.com>
2024-04-28 16:22:39 -0500

    create global variables and templates

--- motion_alarm.ino
+++ motion_alarm.ino
@@ -10,6 +10,13 @@
 SoftwareSerial mySerial(PIN_TFMINI_TX, PIN_TFMINI_RX);      // Uno RX (TFMINI TX), Uno TX (TFMINI RX)
 TFMini tfmini;
 
+// Initialize global vars
+uint16_t minDistance = 0;
+uint16_t distanceBuffer = 5;
+
+void countdown_beeps(uint8_t buzzer_pin, int beeps, unsigned int frequency = 1000, unsigned long beepDuration = 100, unsigned long beepGapDuration = 1000);
+uint16_t read_lidar(TFMini &tfmini, unsigned long readDelay = 50);
+
 void setup() {
   Serial.begin(9600);
   while (!Serial); // wait for serial port to connect. Needed for native USB port only
